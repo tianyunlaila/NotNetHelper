c00089ca70bf647dd69538df3fc346ee14583368
tianyunlaila <1049368931@qq.com>
2017-03-19 21:56:13 +0800

    first

---
+++ git init.cmd
@@ -0,0 +1,7 @@
+for %%i in ("%cd%") do set "name=%%~ni"
+git init
+git remote add origin git@github.com:tianyunlaila/%name%.git
+git add .
+git commit -m "first"
+git push -u origin master
+git status
