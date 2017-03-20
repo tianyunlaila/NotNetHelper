14f729d454605e79fe29ecc7cf94ce2017214c52
tianyunlaila <1049368931@qq.com>
2017-03-20 13:36:16 +0800

    Calendar

--- NotNetHelper/Form1.Designer.cs
+++ NotNetHelper/Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(34, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(434, 264);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1128, 671);
+            this.ClientSize = new System.Drawing.Size(564, 336);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- NotNetHelper/Form1.cs
+++ NotNetHelper/Form1.cs
@@ -16,5 +16,24 @@ namespace NotNetHelper
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ChineseCalendar c = new ChineseCalendar(DateTime.Now);
+            //StringBuilder dayInfo = new StringBuilder();
+            //dayInfo.Append("阳历：" + c.DateString + "\r\n");
+            //dayInfo.Append("农历：" + c.ChineseDateString + "\r\n");
+            //dayInfo.Append("星期：" + c.WeekDayStr);
+            //dayInfo.Append("时辰：" + c.ChineseHour + "\r\n");
+            //dayInfo.Append("属相：" + c.AnimalString + "\r\n");
+            //dayInfo.Append("节气：" + c.ChineseTwentyFourDay + "\r\n");
+            //dayInfo.Append("前一个节气：" + c.ChineseTwentyFourPrevDay + "\r\n");
+            //dayInfo.Append("下一个节气：" + c.ChineseTwentyFourNextDay + "\r\n");
+            //dayInfo.Append("节日：" + c.DateHoliday + "\r\n");
+            //dayInfo.Append("干支：" + c.GanZhiDateString + "\r\n");
+            //dayInfo.Append("星宿：" + c.ChineseConstellation + "\r\n");
+            //dayInfo.Append("星座：" + c.Constellation + "\r\n");
+            //richTextBox1.Text = dayInfo.ToString();
+        }
     }
 }
